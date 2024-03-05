10db8602afc859b9363515400b27ee8a28c62534
lilyfleur111 <157452335+lilyfleur111@users.noreply.github.com>
2024-03-05 15:49:41 +0800

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,16 @@
+HelloWorld
+
+print("Hello World")
+
+Output
+
+x = 25
+message ="Hi"
+ratio = 3.14
+
+print(type(message))
+print(type(ratio))
+print(type(x))
+
+print(message + "My name is Sean. I am" + x + "years old.")
+print(f"{message} my name is Sean. I am {x} years old.")
---
+++ output.py
@@ -0,0 +1,14 @@
+#variable declaration
+x = 22
+message = "hidoo!"
+ratio = 3.14
+
+#checking the type of data with() method.
+print(type(message))
+print(type(ratio))
+print(type(x))
+
+#string formatting
+print(message + "My name is Suzanne. I am  " + str(x) + "years old.")
+print(f"{message} My name is Suzanne. I am {x} years old.")
+
