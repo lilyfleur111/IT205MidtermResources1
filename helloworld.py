5d40b96c1be242aab647d268c028ac3fbc9f7956
lilyfleur111 <157452335+lilyfleur111@users.noreply.github.com>
2024-03-05 15:59:07 +0800

    Add files via upload

--- helloworld.py
+++ helloworld.py
@@ -12,5 +12,5 @@ print(type(message))
 print(type(ratio))
 print(type(x))
 
-print(message + "My name is Sean. I am" + x + "years old.")
-print(f"{message} my name is Sean. I am {x} years old.")
+print(message + "My name is Suzanne. I am" + x + "years old.")
+print(f"{message} my name is Suzanne. I am {x} years old.")
